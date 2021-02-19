95a90e1828f49f6c88e9a6dd553f648043229cc9
Porti <jaime.portilloperez@gmail.com>
2021-02-19 12:39:30 +0100

    font

--- GUI_WIDGETS/Settings/ui_design_settings.py
+++ GUI_WIDGETS/Settings/ui_design_settings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'design_settingssNaduG.ui'
+## Form generated from reading UI file 'design_settingsGvsEIm.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
