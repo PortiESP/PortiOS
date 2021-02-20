bf5dace5e46832739d4165892f2a5fb819244d52
Porti <jaime.portilloperez@gmail.com>
2021-02-20 17:54:10 +0100

    font

--- portiOSEssentials.py
+++ portiOSEssentials.py
@@ -20,6 +20,6 @@ import resources_rc
 # Program imports
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
-from PySide2.QtWidgets import *
+from PySide2.QtCore import *
 import sys, threading, time
 
