649c1b836a3b1597a30f7904dfca38fd9d6ac3b4
Porti <jaime.portilloperez@gmail.com>
2021-02-20 13:11:44 +0100

    font

--- GUI_WIDGETS/Settings/settings_functions.py
+++ GUI_WIDGETS/Settings/settings_functions.py
@@ -4,15 +4,15 @@ class Settings_funcs:
 	def settingsSetup(self, frame):
 		self.frame = frame
 
-		self.frame.bearing_settingsBrightness.clicked.connect(lambda:Settings_funcs.setPage(0))
-		self.frame.bearing_settingsWifi.clicked.connect(lambda:Settings_funcs.setPage(1))
-		self.frame.bearing_settingsBluetooth.clicked.connect(lambda:Settings_funcs.setPage(2))
-		self.frame.bearing_settingsSound.clicked.connect(lambda:Settings_funcs.setPage(3))
-		self.frame.bearing_settingsStatus.clicked.connect(lambda:Settings_funcs.setPage(4))
-		self.frame.bearing_settingsGpio.clicked.connect(lambda:Settings_funcs.setPage(5))
-		self.frame.bearing_settingsSystemInfo.clicked.connect(lambda:Settings_funcs.setPage(6))
-		self.frame.bearing_settingsTerminal.clicked.connect(lambda:Settings_funcs.setPage(7))
-		self.frame.bearing_settingsAdvanced.clicked.connect(lambda:Settings_funcs.setPage(8))
+		self.frame.bearing_settingsBrightness.clicked.connect(lambda:self.setPage(0))
+		self.frame.bearing_settingsWifi.clicked.connect(lambda:self.setPage(1))
+		self.frame.bearing_settingsBluetooth.clicked.connect(lambda:self.setPage(2))
+		self.frame.bearing_settingsSound.clicked.connect(lambda:self.setPage(3))
+		self.frame.bearing_settingsStatus.clicked.connect(lambda:self.setPage(4))
+		self.frame.bearing_settingsGpio.clicked.connect(lambda:self.setPage(5))
+		self.frame.bearing_settingsSystemInfo.clicked.connect(lambda:self.setPage(6))
+		self.frame.bearing_settingsTerminal.clicked.connect(lambda:self.setPage(7))
+		self.frame.bearing_settingsAdvanced.clicked.connect(lambda:self.setPage(8))
 
 	def setPage(self, index):
 		print('Changing to page ',index)
