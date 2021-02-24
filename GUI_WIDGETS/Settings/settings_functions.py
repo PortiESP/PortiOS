ed1ba5cf7110c89303103e61474a7f595a66636e
Porti <jaime.portilloperez@gmail.com>
2021-02-24 19:31:00 +0100

    font

--- GUI_WIDGETS/Settings/settings_functions.py
+++ GUI_WIDGETS/Settings/settings_functions.py
@@ -2,17 +2,17 @@
 
 class Settings_funcs:
 	def settingsSetup(self, frame):
-		self.frame = frame
+		
 
-		self.frame.bearing_settingsBrightness.clicked.connect(lambda:self.setPage(0))
-		self.frame.bearing_settingsWifi.clicked.connect(lambda:self.setPage(1))
-		self.frame.bearing_settingsBluetooth.clicked.connect(lambda:self.setPage(2))
-		self.frame.bearing_settingsSound.clicked.connect(lambda:self.setPage(3))
-		self.frame.bearing_settingsStatus.clicked.connect(lambda:self.setPage(4))
-		self.frame.bearing_settingsGpio.clicked.connect(lambda:self.setPage(5))
-		self.frame.bearing_settingsSystemInfo.clicked.connect(lambda:self.setPage(6))
-		self.frame.bearing_settingsTerminal.clicked.connect(lambda:self.setPage(7))
-		self.frame.bearing_settingsAdvanced.clicked.connect(lambda:self.setPage(8))
+		self.GUI_Settings.bearing_settingsBrightness.clicked.connect(lambda:Settings_funcs.setPage(self, 0))
+		self.GUI_Settings.bearing_settingsWifi.clicked.connect(lambda:Settings_funcs.setPage(self, 1))
+		self.GUI_Settings.bearing_settingsBluetooth.clicked.connect(lambda:Settings_funcs.setPage(self, 2))
+		self.GUI_Settings.bearing_settingsSound.clicked.connect(lambda:Settings_funcs.setPage(self, 3))
+		self.GUI_Settings.bearing_settingsStatus.clicked.connect(lambda:Settings_funcs.setPage(self, 4))
+		self.GUI_Settings.bearing_settingsGpio.clicked.connect(lambda:Settings_funcs.setPage(self, 5))
+		self.GUI_Settings.bearing_settingsSystemInfo.clicked.connect(lambda:Settings_funcs.setPage(self, 6))
+		self.GUI_Settings.bearing_settingsTerminal.clicked.connect(lambda:Settings_funcs.setPage(self, 7))
+		self.GUI_Settings.bearing_settingsAdvanced.clicked.connect(lambda:Settings_funcs.setPage(self, 8))
 
 	def setPage(self, index):
 		print('Changing to page ',index)
