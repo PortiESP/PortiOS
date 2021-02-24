7608128ab57bb2b01965423c999b49636b65c52e
Porti <jaime.portilloperez@gmail.com>
2021-02-25 00:13:02 +0100

    font

--- main.py
+++ main.py
@@ -71,7 +71,8 @@ class Main_GUI:
 		elif str(data[0]) == 'Volume':
 			self.GUI_Central.slider_volume.setValue(int(data[1]))
 
-		Dashboard_funcs.changeMusicInfo(self)
+		elif str(data[0]) == 'Track':
+			Dashboard_funcs.changeMusicInfo(self)
 
 	def startMediaPlayer(self):
 		self.mediaPlayerThread = threading.Thread(target=self.mediaPlayerThreadFunc)
