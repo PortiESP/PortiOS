917677357b6e265f7c188ad99ca0da0281980d9d
Porti <jaime.portilloperez@gmail.com>
2021-02-24 23:56:14 +0100

    font

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ class Main_GUI:
 
 		elif str(data[0]) == 'Volume':
 			self.GUI_Central.slider_volume.setValue(int(data[1]))
-			
+
 		Dashboard_funcs.changeMusicInfo(self)
 
 	def startMediaPlayer(self):
@@ -105,6 +105,7 @@ class Main_GUI:
 				self.GUI_Central.slider_volume.setValue(self.BTController.get_volume_data())
 				if str(self.BTController.get_player_data('Status')) == 'playing':
 					self.toogle_musicStatus(setStatus='playing')
+					Dashboard_funcs.changeMusicInfo()
 				self.BTController.bus.add_signal_receiver(self.mediaDataChanged, 
 											dbus_interface = "org.freedesktop.DBus.Properties",
             								signal_name = "PropertiesChanged",
