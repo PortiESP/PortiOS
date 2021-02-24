0db6cf0d418413b8382c7859a24b3a460201f7de
Porti <jaime.portilloperez@gmail.com>
2021-02-24 21:07:10 +0100

    font

--- GUI_WIDGETS/Dashboard/dashboard_functions.py
+++ GUI_WIDGETS/Dashboard/dashboard_functions.py
@@ -8,6 +8,15 @@ class Dashboard_funcs:
 
 	def changeMusicInfo(self, data):
 		# Setting labels
-		self.GUI_Dashboard.label_cancion.setText(data['Title']) 
-		self.GUI_Dashboard.label_artista.setText(data['Artist'])
+		try:
+			self.GUI_Dashboard.label_duration.setText(Dashboard_funcs.formatDuration(data['Duration']))
+			self.GUI_Dashboard.label_cancion.setText(data['Title']) 
+			self.GUI_Dashboard.label_artista.setText(data['Artist'])
+		except KeyError:
+			pass
 
+	def formatDuration(self, duration):
+		duration /= 1000
+		mins = duration // 60
+		segs = int((duration % 60) * 60)
+		return str(mins) + ':' + str(segs)
