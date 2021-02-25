58def35a7a140f6861095a0c8f43804ca0e45ac8
Porti <jaime.portilloperez@gmail.com>
2021-02-25 01:35:52 +0100

    font

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ class Main_GUI:
 			self.GUI_Central.slider_volume.setValue(int(values))
 		
 
-		Dashboard_funcs.changeMusicInfo(self, data=(key, values))
+		Dashboard_funcs.changeMusicInfo(self, data=(key, list(values)[0]))
 
 
 	def startMediaPlayer(self):
