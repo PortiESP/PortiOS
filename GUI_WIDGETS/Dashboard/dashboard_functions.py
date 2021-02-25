c4e20bfe675f4d5d2989044517f4f2ae833cb1cb
Porti <jaime.portilloperez@gmail.com>
2021-02-25 15:40:45 +0100

    font

--- GUI_WIDGETS/Dashboard/dashboard_functions.py
+++ GUI_WIDGETS/Dashboard/dashboard_functions.py
@@ -12,7 +12,7 @@ class Dashboard_funcs:
 			if len(track) == 1:
 				self.trackDuration = track['Duration']
 				self.GUI_Dashboard.label_duration.setText(str(Dashboard_funcs.formatDuration( self.trackDuration)))
-				self.GUI_Dashboard.slider_duration.setMaximum(Dashboard_funcs.formatDuration( self.trackDuration))
+				self.GUI_Dashboard.slider_duration.setMaximum(int(self.trackDuration/1000))
 
 			self.GUI_Dashboard.label_cancion.setText(str(track['Title']))
 			self.GUI_Dashboard.label_artista.setText(str(track['Artist']))	 
