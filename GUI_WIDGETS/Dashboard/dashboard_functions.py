c50747060018250a7a4b67ede91c0d0f208359ad
Porti <jaime.portilloperez@gmail.com>
2021-02-26 18:55:00 +0100

    font

--- GUI_WIDGETS/Dashboard/dashboard_functions.py
+++ GUI_WIDGETS/Dashboard/dashboard_functions.py
@@ -1,5 +1,7 @@
 from GUI_WIDGETS.Dashboard.gauge_functions import *
-from math import ceil
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
 
 class Dashboard_funcs:
 
