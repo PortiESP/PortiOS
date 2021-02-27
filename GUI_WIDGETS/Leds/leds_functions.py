45074a6352c48ac98680d989d54b8d8c930e1485
Porti <jaime.portilloperez@gmail.com>
2021-02-27 17:25:24 +0100

    font

--- GUI_WIDGETS/Leds/leds_functions.py
+++ GUI_WIDGETS/Leds/leds_functions.py
@@ -6,6 +6,9 @@ class Leds_funcs:
 	def ledsSetup(self):
 		# Color 
 		self.GUI_Leds.ledColor = [0,0,0]
+		self.GUI_Leds.slidersList = (self.GUI_Leds.ledsSliderRed,
+									 self.GUI_Leds.ledsSliderGreen,
+									 self.GUI_Leds.ledsSliderBlue)
 
 		# Programs buttons
 		# self.GUI_Leds.ledsProgramJumpButton.clicked.connect()
@@ -15,7 +18,7 @@ class Leds_funcs:
 		# self.GUI_Leds.ledsProgramPoliceButton.clicked.connect()
 
 		# Color picker
-		# self.GUI_Leds.ledsColorPickerButton.clicked.connect()
+		self.GUI_Leds.ledsColorPickerButton.clicked.connect(lambda: Leds_funcs.pickColor(self))
 
 		# On/off Button
 		# self.GUI_Leds.ledsOnOffButton.clicked.connect()
@@ -30,12 +33,28 @@ class Leds_funcs:
 	def colorValueSetup(self, index):
 		if index == 0:
 			self.GUI_Leds.ledColor[0] = self.GUI_Leds.ledsSliderRed.value()
-			self.GUI_Leds.ledsValueRed.setText(str(self.GUI_Leds.ledsSliderRed.value()))
+			self.GUI_Leds.ledsValueRed.setText(str(self.GUI_Leds.ledColor[0]))
 		elif index == 1:
 			self.GUI_Leds.ledColor[1] = self.GUI_Leds.ledsSliderGreen.value()
-			self.GUI_Leds.ledsValueGreen.setText(str(self.GUI_Leds.ledsSliderGreen.value()))
+			self.GUI_Leds.ledsValueGreen.setText(str(self.GUI_Leds.ledColor[1]))
 		elif index == 2:
 			self.GUI_Leds.ledColor[2] = self.GUI_Leds.ledsSliderBlue.value()
-			self.GUI_Leds.ledsValueBlue.setText(str(self.GUI_Leds.ledsSliderBlue.value()))
-
+			self.GUI_Leds.ledsValueBlue.setText(str(self.GUI_Leds.ledColor[2]))
+		
 		self.GUI_Leds.ledsBulb.setStyleSheet(u"background:rgb({},{},{});".format(*self.GUI_Leds.ledColor))
+
+
+
+	def pickColor(self):
+		# Getting color without alpha
+		color = QColorDialog.getColor()[1:]
+
+		# Setting slider value and set values
+		for i in range(3): 
+			self.GUI_Leds.slidersList[i].setValue(color[i])
+			Led_funcs.colorValueSetup(self, i)
+
+
+
+
+
