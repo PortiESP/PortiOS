ed376faba6a842cae3cd6df9b9c206da35dd19f2
Porti <jaime.portilloperez@gmail.com>
2021-03-02 14:19:24 +0100

    font

--- GUI_WIDGETS/Settings/settings_functions.py
+++ GUI_WIDGETS/Settings/settings_functions.py
@@ -63,7 +63,7 @@ class Settings_funcs:
 			out = subprocess.run('ifconfig wlan0', capture_output=True, text=True, shell=True).stdout.split('\n')[1].strip().split(' ')
 			if out[0] == 'inet': 
 				out = out[1]
-				return True
+				return out
 			else: return False
 			print('IP: ', out)
 			
