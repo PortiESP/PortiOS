1b8cab68d9a4f2a630be60de4bbb28c40e3a12a1
Porti <jaime.portilloperez@gmail.com>
2021-03-03 01:04:44 +0100

    font

--- GUI_WIDGETS/Settings/settings_functions.py
+++ GUI_WIDGETS/Settings/settings_functions.py
@@ -72,15 +72,7 @@ class Settings_funcs:
 			
 		def connectWifi():
 			print('Attempting to connect to "', self.GUI_Settings.bearing_wifiSSIDInput.text(), '" with key "', self.GUI_Settings.bearing_wifiPassInput.text(), '"')
-			command = 'echo ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev	\n\
-						update_config=1											\n\
-						country=ES 												\n\
-						     													\n\
-						network={{												\n\
-						    ssid="{}"											\n\
-						    psk="{}"											\n\
-						    key_mgmt=WPA-PSK									\n\
-						}} > /etc/wpa_supplicant/wpa_supplicant.conf'
+			command = 'echo "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\ncountry=ES\n\nnetwork={{\nssid="{}"\npsk="{}"\nkey_mgmt=WPA-PSK\n}} "> /etc/wpa_supplicant/wpa_supplicant.conf'
 			out = subprocess.run(command.format(self.GUI_Settings.bearing_wifiSSIDInput.text(), self.GUI_Settings.bearing_wifiPassInput.text()), capture_output=True, shell=True)
 			out2 = subprocess.run('sudo wpa_cli -i wlan0 reconfigure', capture_output=True, shell=True)
 			if out.returncode == 0 and out2.stdout == 'OK':
