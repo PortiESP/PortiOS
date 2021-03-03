f3fcd8087036b51a36cb2b369d799e4b3ced693e
Porti <jaime.portilloperez@gmail.com>
2021-03-03 01:24:53 +0100

    font

--- GUI_WIDGETS/Settings/settings_functions.py
+++ GUI_WIDGETS/Settings/settings_functions.py
@@ -78,7 +78,8 @@ class Settings_funcs:
 				f.write(command.format(self.GUI_Settings.bearing_wifiSSIDInput.text(), self.GUI_Settings.bearing_wifiPassInput.text()))
 			
 			out = subprocess.run('sudo wpa_cli -i wlan0 reconfigure', capture_output=True, text=True, shell=True)
-			if out.stderr == 'OK':
+			print(out.stdout)
+			if out.stdout == 'OK':
 				print('Connection success')
 				refresh()
 			else:
