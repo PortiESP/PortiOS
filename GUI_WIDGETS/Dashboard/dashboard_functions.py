cfe30e9372a5edef1cff30f8744b9228e31d38d3
Porti <jaime.portilloperez@gmail.com>
2021-03-06 14:46:26 +0100

    font

--- GUI_WIDGETS/Dashboard/dashboard_functions.py
+++ GUI_WIDGETS/Dashboard/dashboard_functions.py
@@ -3,6 +3,7 @@ from .gauge_functions import *
 from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
+import treading, Adafruit_ADS1x15
 
 class Dashboard_funcs:
 
@@ -10,9 +11,18 @@ class Dashboard_funcs:
 		# Getting interface
 		self.GUI_Dashboard = Ui_Dashboard_widget()
 		self.GUI_Dashboard.setupUi(self.GUI_Central.page_dashboard)
+
+		# ADC converter
+		adc = Adafruit_ADS1x15.ADS1115()
+		self.GUI_Dashboard.GAUGE_ADS_CHANNEL = 0
+		self.GUI_Dashboard.MAX_ADS_VALUE = 25200
 		
 		# Getting gauge funcs
 		Gauge_funcs.setDefaults(self)
+
+		self.GUI_Dashboard.gaugeThread = None
+		self.GUI_Dashboard.gaugePower = False
+		Dashboard_funcs.startGauge(self)
 
 
 	def changeMusicInfo(self):
@@ -34,3 +44,17 @@ class Dashboard_funcs:
 	def mapDigitalSpeed(self, value, maxValue=200):
 		value = (value / maxValue) * self.GUI_Dashboard.TOP_SPEED
 		return value
+
+	def startGauge(self):
+		def gaugeThreadFunc():
+			while self.GUI_Dashboard.gaugePower:
+				value = adc.read_adc(self.GUI_Dashboard.GAUGE_ADS_CHANNEL, gain=2, data_rate=16)
+				value = Dashboard_funcs.mapDigitalSpeed(self, value, maxValue=self.GUI_Dashboard.MAX_ADS_VALUE)
+				Gauge_funcs.setSpeed(self, value)
+
+		self.GUI_Dashboard.gaugePower = True
+		self.GUI_Dashboard.gaugeThread = threading.Thread(target=gaugeThreadFunc)
+		self.GUI_Dashboard.gaugeThread.start()
+
+	def stopGauge(self):
+		self.GUI_Dashboard.gaugePower = False
