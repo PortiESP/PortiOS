cfb6bfc0f65fb7fa83dd6c263a17952eaf36cc62
Porti <jaime.portilloperez@gmail.com>
2021-03-06 01:56:00 +0100

    font

--- GUI_PACKAGES/RGB_led/rgb_controller.py
+++ GUI_PACKAGES/RGB_led/rgb_controller.py
@@ -151,6 +151,7 @@ class RGB_Controller:
 			
 	def program_stop(self, program='self'):
 		if program == 'self': program = self.loop_thread
+		program.join()
 		self.loop_thread = None
 		self.set_off()
 
