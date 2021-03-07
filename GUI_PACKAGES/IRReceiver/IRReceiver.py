5849edd68b4b1223d931152732e5e25496787c42
Porti <jaime.portilloperez@gmail.com>
2021-03-07 23:36:46 +0100

    font

--- GUI_PACKAGES/IRReceiver/IRReceiver.py
+++ GUI_PACKAGES/IRReceiver/IRReceiver.py
@@ -32,11 +32,12 @@ class IRReceiver:
 		if data == 0:
 			duration = time.time() - self.bit_start
 			if duration < 0.005:
-				self.bits_durations_list.append(duration)
+				print(len(self.bits_durations_list), ' - ',duration)
+				# self.bits_durations_list.append(duration)
 			else:
 				print('start')
 			if len(self.bits_durations_list) == 32:
-				self.IRReceiverCallback(self.bits_durations_list)
+				# self.IRReceiverCallback(self.bits_durations_list)
 				print('end')
 				self.bits_durations_list = []
 
